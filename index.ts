5c9f4e8c7f76534e3667b55bd5d97252d6822b7c
Ximena Cordero <cor.xime@gmail.com>
2026-05-26 16:37:13 +0200

    Tarea 5 - Logger and shopping cart with sessions completed

--- index.ts
+++ index.ts
@@ -1,9 +1,36 @@
 import express from "express"
 import nunjucks from "nunjucks"
+import logger from "./logger.ts"
+import session from "express-session"
 
 import ProductosRouter from "./routes/productos.ts"
 
 const app = express()
+
+// Middleware formularios
+app.use(express.urlencoded({
+    extended: true
+}))
+
+// Middleware sesiones
+app.use(session({
+
+    secret: "my-secret",
+
+    resave: false,
+
+    saveUninitialized: false
+
+}))
+
+app.use((req: any, res, next) => {
+
+    res.locals.total_carrito =
+        req.session.total_carrito || 0
+
+    next()
+
+})
 
 const PORT = 3000
 
@@ -21,6 +48,8 @@ app.use("/public/imagenes", express.static("imagenes"))
 
 // Rutas
 app.use("/", ProductosRouter)
+
+logger.info("Servidor iniciando...")
 
 app.listen(PORT, () => {
     console.log(`Servidor funcionando en http://localhost:${PORT}`)
--- routes/productos.ts
+++ routes/productos.ts
@@ -1,6 +1,7 @@
 import express from "express"
 
 import prisma from "../prisma/prisma.client.ts"
+import logger from "../logger.ts"
 
 const router = express.Router()
 
@@ -79,6 +80,50 @@ router.get("/buscar", async (req, res) => {
         res.status(500).send(error.message)
 
     }
+
+})
+
+router.post("/al-carrito/:id", async (req: any, res) => {
+
+    const id = Number(req.params.id)
+
+    const cantidad = Number(req.body.cantidad)
+
+    logger.info(
+        `Producto ${id} añadido al carrito (${cantidad})`
+    )
+
+    if (cantidad > 0) {
+
+        if (req.session.carrito !== undefined) {
+
+            req.session.carrito.push({
+                id,
+                cantidad
+            })
+
+        } else {
+
+            req.session.carrito = [{
+                id,
+                cantidad
+            }]
+
+        }
+
+        let total_carrito = 0
+
+        for (const item of req.session.carrito) {
+
+            total_carrito += item.cantidad
+
+        }
+
+        req.session.total_carrito = total_carrito
+
+    }
+
+    res.redirect(`/producto/${id}`)
 
 })
 
